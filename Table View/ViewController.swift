059ddc3bec3e56d196a09d29c04cb2ad89b133cc
itsrishav <with.rishav@gmail.com>
2016-12-05 17:20:23 +0530

    Learnt About Table View 1. Rows and Cells How to Integrate them to ViewController 2. How To reload Table Content in use with Slider

--- Table View/ViewController.swift
+++ Table View/ViewController.swift
@@ -8,7 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var slider: UISlider!
+    
+    @IBOutlet var table: UITableView!
+    @IBAction func sliderChanged(_ sender: AnyObject) {
+            table.reloadData()
+        
+    }
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "\(Int(slider.value) * (indexPath.row + 1))"
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
